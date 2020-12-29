b75a3b94379763e110ba155c0c0a54e717fb7306
forestik <orest199910@gmail.com>
2020-12-29 12:35:17 +0200

    User microservice

---
+++ user/src/main/java/greencity/constant/LogMessage.java
@@ -0,0 +1,8 @@
+package greencity.constant;
+
+public final class LogMessage {
+    public static final String IN_FIND_ID_BY_EMAIL = "in findIdByEmail(), email: {}";
+
+    private LogMessage() {
+    }
+}
