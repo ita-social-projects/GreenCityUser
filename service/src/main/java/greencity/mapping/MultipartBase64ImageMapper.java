17cef653e0f80a2244371f090bf9a451088490e5
Woland <gutejvova@gmail.com>
2021-04-13 00:08:03 +0300

    system.out -> System.err

--- service/src/main/java/greencity/mapping/MultipartBase64ImageMapper.java
+++ service/src/main/java/greencity/mapping/MultipartBase64ImageMapper.java
@@ -35,9 +35,9 @@ public class MultipartBase64ImageMapper extends AbstractConverter<String, Multip
             BufferedImage bufferedImage = ImageIO.read(bis);
             ImageIO.write(bufferedImage, "png", tempFile);
             FileItem fileItem = new DiskFileItem("mainFile", Files.probeContentType(tempFile.toPath()),
-                    false, tempFile.getName(), (int) tempFile.length(), tempFile.getParentFile());
+                false, tempFile.getName(), (int) tempFile.length(), tempFile.getParentFile());
             try (InputStream input = new FileInputStream(tempFile);
-                 OutputStream outputStream = fileItem.getOutputStream()) {
+                OutputStream outputStream = fileItem.getOutputStream()) {
                 IOUtils.copy(input, outputStream);
                 outputStream.flush();
                 return new CommonsMultipartFile(fileItem);
