c0b173cb1920d899771772f9140ab5140d1c82d6
NazariySt <struk.nazariy@gmail.com>
2021-04-22 13:40:03 +0300

    fixed code smell

--- service-api/src/main/java/greencity/dto/UbsCustomerDto.java
+++ service-api/src/main/java/greencity/dto/UbsCustomerDto.java
@@ -1,7 +1,6 @@
 package greencity.dto;
 
 import lombok.*;
-import org.springframework.context.annotation.Role;
 
 @Getter
 @Setter
--- service/src/test/java/greencity/mapping/UbsCustomerMapperTest.java
+++ service/src/test/java/greencity/mapping/UbsCustomerMapperTest.java
@@ -11,7 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class UbsCustomerMapperTest {
+class UbsCustomerMapperTest {
 
     @InjectMocks
     private UbsCustomerMapper customerMapper;
