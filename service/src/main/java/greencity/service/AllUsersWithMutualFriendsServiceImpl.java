be0acf4fe515b2b08fba01b127664d90768baa9a
NazariySt <struk.nazariy@gmail.com>
2021-05-12 15:09:40 +0300

    Controller for getting all Users with mutual friends v4.0

--- service/src/main/java/greencity/service/AllUsersWithMutualFriendsServiceImpl.java
+++ service/src/main/java/greencity/service/AllUsersWithMutualFriendsServiceImpl.java
@@ -18,6 +18,7 @@ public class AllUsersWithMutualFriendsServiceImpl implements AllUsersMutualFrien
     private final JdbcTemplate jdbcTemplate;
     private final RestClient restClient;
 
+    @SuppressWarnings("CheckStyle")
     @Override
     public PageableDto<UserAllFriendsDto> findAllUsersWithMutualFriends(Long id, int pages, int size) {
         int offset = pages * size;
@@ -29,8 +30,8 @@ public class AllUsersWithMutualFriendsServiceImpl implements AllUsersMutualFrien
                 + "WHERE U1.user_id =" + id + " GROUP BY U2.user_id Having u2.user_id not in (" + id + ")\n"
                 + "ORDER BY MUTUAL_COUNT DESC) u2 JOIN users u1 on u2.user_id = u1.id\n"
                 + "LIMIT " + size + " OFFSET " + offset);
-        int numberOfElements =
-            jdbcTemplate.queryForObject("SELECT count(*) FROM (SELECT U2.USER_ID, COUNT(*) AS MUTUAL_COUNT"
+        int numberOfElements = 0;
+          numberOfElements =  jdbcTemplate.queryForObject("SELECT count(*) FROM (SELECT U2.USER_ID, COUNT(*) AS MUTUAL_COUNT"
                 + " FROM users_friends U1\n"
                 + "LEFT JOIN users_friends U2 on U1.friend_id = U2.friend_id\n"
                 + "left join users on users.id = u2.user_id\n"
