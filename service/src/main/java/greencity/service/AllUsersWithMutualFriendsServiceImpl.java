c7c0b0dccf0e4dea4f2291b6f0e6f13d50bd713d
NazariySt <struk.nazariy@gmail.com>
2021-05-12 15:45:02 +0300

    Controller for getting all Users with mutual friends v7.0

--- service/src/main/java/greencity/service/AllUsersWithMutualFriendsServiceImpl.java
+++ service/src/main/java/greencity/service/AllUsersWithMutualFriendsServiceImpl.java
@@ -3,6 +3,7 @@ package greencity.service;
 import greencity.client.RestClient;
 import greencity.dto.PageableDto;
 import greencity.dto.user.UserAllFriendsDto;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -18,23 +19,27 @@ public class AllUsersWithMutualFriendsServiceImpl implements AllUsersMutualFrien
 
     @Override
     public PageableDto<UserAllFriendsDto> findAllUsersWithMutualFriends(Long id, int pages, int size) {
+        int numberOfElements = 0;
         int offset = pages * size;
         List<Map<String, Object>> maps = null;
-        maps = jdbcTemplate.queryForList("SELECT * FROM (SELECT U2.USER_ID, COUNT(*) AS MUTUAL_COUNT"
-            + " FROM users_friends U1\n"
-            + "LEFT JOIN users_friends U2 on U1.friend_id = U2.friend_id\n"
-            + "left join users on users.id = u2.user_id\n"
-            + "WHERE U1.user_id =" + id + " GROUP BY U2.user_id Having u2.user_id not in (" + id + ")\n"
-            + "ORDER BY MUTUAL_COUNT DESC) u2 JOIN users u1 on u2.user_id = u1.id\n"
-            + "LIMIT " + size + " OFFSET " + offset);
-        int numberOfElements = 0;
-        numberOfElements = jdbcTemplate.queryForObject("SELECT count(*) "
-            + " FROM (SELECT U2.USER_ID, COUNT(*) AS MUTUAL_COUNT"
-            + " FROM users_friends U1\n"
-            + "LEFT JOIN users_friends U2 on U1.friend_id = U2.friend_id\n"
-            + "left join users on users.id = u2.user_id\n"
-            + "WHERE U1.user_id =" + id + " GROUP BY U2.user_id Having u2.user_id not in (" + id + ")\n"
-            + "ORDER BY MUTUAL_COUNT DESC) u2 JOIN users u1 on u2.user_id = u1.id\n", Integer.class);
+        try {
+            maps = jdbcTemplate.queryForList("SELECT * FROM (SELECT U2.USER_ID, COUNT(*) AS MUTUAL_COUNT"
+                + " FROM users_friends U1\n"
+                + "LEFT JOIN users_friends U2 on U1.friend_id = U2.friend_id\n"
+                + "left join users on users.id = u2.user_id\n"
+                + "WHERE U1.user_id =" + id + " GROUP BY U2.user_id Having u2.user_id not in (" + id + ")\n"
+                + "ORDER BY MUTUAL_COUNT DESC) u2 JOIN users u1 on u2.user_id = u1.id\n"
+                + "LIMIT " + size + " OFFSET " + offset);
+            numberOfElements = jdbcTemplate.queryForObject("SELECT count(*) "
+                + " FROM (SELECT U2.USER_ID, COUNT(*) AS MUTUAL_COUNT"
+                + " FROM users_friends U1\n"
+                + "LEFT JOIN users_friends U2 on U1.friend_id = U2.friend_id\n"
+                + "left join users on users.id = u2.user_id\n"
+                + "WHERE U1.user_id =" + id + " GROUP BY U2.user_id Having u2.user_id not in (" + id + ")\n"
+                + "ORDER BY MUTUAL_COUNT DESC) u2 JOIN users u1 on u2.user_id = u1.id\n", Integer.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         List<UserAllFriendsDto> result = new ArrayList<>();
         for (Map<String, Object> map : maps) {
             result.add(UserAllFriendsDto.builder()
