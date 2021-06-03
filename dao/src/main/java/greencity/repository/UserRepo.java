9301e798c642ecfa9e45e00bef86966ebc712ee2
KwintiukN <83454713+KwintiukN@users.noreply.github.com>
2021-06-03 11:58:29 +0000

    Deploying to gh-pages from  @ 9faca58d81d49318ac3ead53ed992701047dca17 🚀

--- dao/src/main/java/greencity/repository/UserRepo.java
+++ dao/src/main/java/greencity/repository/UserRepo.java
@@ -319,16 +319,15 @@ public interface UserRepo extends JpaRepository<User, Long>, JpaSpecificationExe
     /**
      * Method that finds top 6 user's recommended friends.
      *
-     * @param pageable {@link Pageable}.
-     * @param userId   {@link Long} - current user's id.
-     * @return {@link Page} of {@link UsersFriendDto} instances.
+     * @param userId {@link Long} - current user's id.
+     * @return {@link List} of {@link UsersFriendDto} instances.
      */
     @Query(nativeQuery = true, value = "select * from users u where u.id not in "
         + "((select friend_id from users_friends where user_id = :userId) "
         + "union (select user_id from users_friends where friend_id = :userId) "
         + "union (select :userId)) "
-        + "order by u.rating desc ")
-    Page<UsersFriendDto> findAnyRecommendedFriends(Pageable pageable, @Param("userId") Long userId);
+        + "order by u.rating desc limit 6;")
+    List<UsersFriendDto> findAnyRecommendedFriends(@Param("userId") Long userId);
 
     /**
      * Method that finds user.
