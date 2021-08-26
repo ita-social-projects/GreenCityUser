dc137c7f3abedf0cb7dc92d734adb28455aa2e21
yurabh <48449309+yurabh@users.noreply.github.com>
2021-08-26 20:11:47 +0000

    Deploying to gh-pages from  @ e8901b7f72cf364334385f51c5e368566a395da3 🚀

--- service/src/test/java/greencity/TestConst.java
+++ service/src/test/java/greencity/TestConst.java
@@ -5,4 +5,6 @@ public final class TestConst {
     public static final String NAME = "Taras";
     public static final String EMAIL = "taras@gmail.com";
     public static final String IMG_NAME = "Screenshot.png";
+    public static final Long SIMPLE_LONG_NUMBER = 1L;
+    public static final Long SIMPLE_LONG_NUMBER_BAD_VALUE = 100L;
 }
