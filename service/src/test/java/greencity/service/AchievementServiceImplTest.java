2753993db40d59acba49b5457e88981289878468
StepanTehlivets <71794544+StepanTehlivets@users.noreply.github.com>
2021-08-26 09:52:35 +0000

    Deploying to gh-pages from  @ 2f6746305acf86a049916354f924d1681ad2b691 🚀

--- service/src/test/java/greencity/service/AchievementServiceImplTest.java
+++ service/src/test/java/greencity/service/AchievementServiceImplTest.java
@@ -1,16 +1,24 @@
 package greencity.service;
 
+import greencity.ModelUtils;
+import greencity.dto.achievement.AchievementVO;
 import greencity.entity.Achievement;
 import greencity.entity.User;
 import greencity.entity.UserAchievement;
 import greencity.enums.AchievementStatus;
+import greencity.repository.AchievementRepo;
 import greencity.repository.UserAchievementRepo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -22,6 +30,12 @@ class AchievementServiceImplTest {
 
     @Mock
     private UserAchievementRepo userAchievementRepo;
+
+    @Mock
+    private AchievementRepo achievementRepo;
+
+    @Mock
+    private ModelMapper modelMapper;
 
     @Test
     void findUserAchievement() {
@@ -39,4 +53,16 @@ class AchievementServiceImplTest {
         verify(userAchievementRepo).save(userAchievement);
 
     }
+
+    @Test
+    void findAllTest() {
+        List<Achievement> achievementVOS = new ArrayList<>();
+        achievementVOS.add(ModelUtils.getAchievement());
+        when(achievementRepo.findAll()).thenReturn(achievementVOS);
+
+        List<AchievementVO> allAchievements = achievementService.findAll();
+        assertFalse(allAchievements.isEmpty());
+
+    }
+
 }
