8e0e071d40b3a8114a99bff12b07086b6fd48269
LiubomyrPater <pater87lm@gmail.com>
2021-09-24 14:04:51 +0300

    fix creating order on UBS

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -131,7 +131,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/user/findByEmail",
                 "/user/findIdByEmail",
                 "/user/findById",
-                "user/findByUuId",
+                "/user/findByUuId",
                 "/user/findUuidByEmail",
                 "/user/createUbsRecord")
             .hasAnyRole(USER, ADMIN, MODERATOR)
