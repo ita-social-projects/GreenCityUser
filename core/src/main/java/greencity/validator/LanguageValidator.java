4719a5a0369581493d688d3ee3bb92a1bdd807ee
vmakitra <v-vmakitra@expediagroup.com>
2022-05-23 21:16:09 +0300

    Added fall back strategy to the registration

--- core/src/main/java/greencity/validator/LanguageValidator.java
+++ core/src/main/java/greencity/validator/LanguageValidator.java
@@ -1,22 +1,32 @@
 package greencity.validator;
 
+import java.util.List;
+import java.util.Locale;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import greencity.annotations.ValidLanguage;
 import greencity.client.RestClient;
 import lombok.AllArgsConstructor;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-import java.util.List;
-import java.util.Locale;
-
 @AllArgsConstructor
 public class LanguageValidator implements ConstraintValidator<ValidLanguage, Locale> {
+    private Logger logger = LoggerFactory.getLogger(LanguageValidator.class);
     private List<String> codes;
     private final RestClient restClient;
 
     @Override
     public void initialize(ValidLanguage constraintAnnotation) {
-        codes = restClient.getAllLanguageCodes();
+        try {
+            codes = restClient.getAllLanguageCodes();
+        } catch (Exception e) {
+            logger.warn("Occurred error during processing request: {}",e.getMessage());
+            codes = List.of("en", "ua");
+        }
     }
 
     @Override
