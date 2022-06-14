6212b86e0cc6f5d192496c3f217aa2fc6512b498
Andrii Yezenitskyi <andrii.yezenitskyi@gmail.com>
2022-06-14 19:11:14 +0300

    fixed code smells

--- service/src/test/java/greencity/service/kafka/UserActionMessagingServiceTest.java
+++ service/src/test/java/greencity/service/kafka/UserActionMessagingServiceTest.java
@@ -6,12 +6,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserActionMessagingServiceTest {
     @InjectMocks
     private UserActionMessagingService service;
+
+    @Value("user.actions")
+    private String topic;
 
     @Mock
     private KafkaTemplate<String, Object> kafkaTemplate;
@@ -19,10 +25,12 @@ class UserActionMessagingServiceTest {
     @Test
     void sendRegistrationEvent() {
         service.sendRegistrationEvent(ModelUtils.getUser());
+        verify(kafkaTemplate).send(eq(topic), any());
     }
 
     @Test
     void sendFriendAddedEvent() {
         service.sendFriendAddedEvent(ModelUtils.getUser(), ModelUtils.getUser());
+        verify(kafkaTemplate, times(2)).send(eq(topic), any());
     }
 }
