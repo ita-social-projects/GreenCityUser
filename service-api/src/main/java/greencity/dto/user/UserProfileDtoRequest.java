85a3a6e5ac310ee532a4d5cfd2e256427be35797
Maksym Golik <maksym.golik@gmail.com>
2023-01-24 15:42:08 +0200

    fixed imports

--- service-api/src/main/java/greencity/dto/user/UserProfileDtoRequest.java
+++ service-api/src/main/java/greencity/dto/user/UserProfileDtoRequest.java
@@ -1,8 +1,14 @@
 package greencity.dto.user;
 
 import greencity.annotations.ValidSocialNetworkLinks;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.Builder;
+
 import java.util.List;
-import lombok.*;
 
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -15,7 +21,8 @@ import javax.validation.constraints.Size;
 @Builder
 public class UserProfileDtoRequest {
     @Size(min = 1, max = 30, message = "name must have no less than 1 and no more than 30 symbols")
-    @Pattern(regexp = "^(?=.{1,30}$)([ЁёІіЇїҐґЄєА-Яа-яA-Za-z-'\\d\\s]+.?[ЁёІіЇїҐґЄєА-Яа-яA-Za-z-'\\d\\s]+){1,5}$",
+    @Pattern(regexp = "^(?=.{1,30}$)([ЁёІіЇїҐґЄєА-Яа-яA-Za-z-'\\d\\s]+.?"+
+            "[ЁёІіЇїҐґЄєА-Яа-яA-Za-z-'\\d\\s]+){1,5}$",
         message = "name must contain only \"ЁёІіЇїҐґЄєА-Яа-яA-Za-z-'0-9 .\", dot can only be in the center of the name")
     private String name;
     @Size(min = 1, max = 85)
