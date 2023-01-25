6d5c829bd5ba15aa9a3dcb41ac8c2237524629d0
MaksymGolik <99403738+MaksymGolik@users.noreply.github.com>
2023-01-25 12:04:06 +0000

    Deploying to gh-pages from  @ bcd85c8c5118bc3028c694a6b911470fdc77dc06 🚀

--- service-api/src/main/java/greencity/dto/user/UserProfileDtoRequest.java
+++ service-api/src/main/java/greencity/dto/user/UserProfileDtoRequest.java
@@ -1,8 +1,14 @@
 package greencity.dto.user;
 
 import greencity.annotations.ValidSocialNetworkLinks;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.Builder;
+
 import java.util.List;
-import lombok.*;
 
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -14,11 +20,14 @@ import javax.validation.constraints.Size;
 @EqualsAndHashCode
 @Builder
 public class UserProfileDtoRequest {
-    @Size(min = 4, max = 30, message = "name must have no less than 4 and no more than 30 symbols")
-    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-яA-Za-z-'\\s.]{4,30}",
-        message = "name must contain only \"ЁёІіЇїҐґЄєА-Яа-яA-Za-z-' .\"")
+    @Size(min = 1, max = 30, message = "name must have no less than 1 and no more than 30 symbols")
+    @Pattern(regexp = "^(?!\\.)(?!.*\\.$)(?!.*?\\.\\.)(?!.*?\\-\\-)(?!.*?\\'\\')[-'ʼ ґҐіІєЄїЇА-Яа-я+\\w.]{1,30}$",
+        message = "name must contain only \"ЁёІіЇїҐґЄєА-Яа-яA-Za-z-'0-9 .\", dot can only be in the center of the name")
     private String name;
+    @Size(min = 1, max = 85)
+    @Pattern(regexp = "[ЁёІіЇїҐґЄєА-Яа-яA-Za-z-'\\s)(!,]{1,85}")
     private String city;
+    @Size(max = 170)
     private String userCredo;
     @ValidSocialNetworkLinks
     private List<String> socialNetworks;
