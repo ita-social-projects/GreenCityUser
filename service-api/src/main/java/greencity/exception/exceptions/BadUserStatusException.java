3c7373e38f6179d9e9e562d4361733c10a5130ad
Kharchenko Volodymyr <kharchenko_ak19@nuwm.edu.ua>
2023-02-06 13:27:22 +0200

    Validated BadUserStatusException

--- service-api/src/main/java/greencity/exception/exceptions/BadUserStatusException.java
+++ service-api/src/main/java/greencity/exception/exceptions/BadUserStatusException.java
@@ -1,11 +1,16 @@
 package greencity.exception.exceptions;
 
 /**
- * Exception that user status is wrong.
+ * Exception that user status is wrong for login.
+ *
+ * @author Volodymyr Kharchenko
+ * @version 1.0
  */
 public class BadUserStatusException extends RuntimeException{
     /**
-     * Constructor.
+     * Constructor for BadUserStatusException.
+     *
+     * @param message - giving message.
      */
     public BadUserStatusException(String message) {
         super(message);
