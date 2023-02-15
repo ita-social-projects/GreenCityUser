d552c7b69a9938961b7f2fb0c5c28665c5e54cde
NikitaKorzh <nikitakorzh10@gmail.com>
2023-02-15 13:06:34 +0200

    added tests

--- service/src/test/java/greencity/security/service/AuthorityServiceImplTest.java
+++ service/src/test/java/greencity/security/service/AuthorityServiceImplTest.java
@@ -2,18 +2,18 @@ package greencity.security.service;
 
 import greencity.dto.UpdateEmployeeAuthoritiesDto;
 import greencity.dto.position.PositionDto;
-import greencity.dto.user.UserEmployeeAuthorityDto;
 import greencity.entity.Authority;
 import greencity.entity.User;
+import greencity.enums.Role;
 import greencity.exception.exceptions.BadRequestException;
-import greencity.exception.exceptions.NotFoundException;
 import greencity.repository.AuthorityRepo;
 import greencity.repository.UserRepo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -21,10 +21,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static greencity.ModelUtils.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -49,20 +52,32 @@ class AuthorityServiceImplTest {
         verify(authorityRepo).getAuthoritiesByEmployeeId(getUser().getId());
     }
 
-//    @Test
-//    void updateEmployeesAuthoritiesTest() {
-//        User user = createUbsAdmin();
-//        User employee = createEmployee();
-//        Authority authority = getAuthority();
-//
-//        when(userRepo.findByEmail("taras@gmail.com")).thenReturn(Optional.of(employee));
-//        when(userRepo.findByEmail("email@mail.com")).thenReturn(Optional.of(user));
-//        when(authorityRepo.findByName("test")).thenReturn(Optional.of(authority));
-//
-//        authorityService.updateEmployeesAuthorities(getUserEmployeeAuthorityDto());
-//        authority.getEmployees().add(createAdmin());
-//        verify(authorityRepo).save(authority);
-//    }
+    @Test
+    void updateEmployeesAuthoritiesTest() {
+        User employee = createEmployee();
+        List<Authority> authority = List.of(getAuthority());
+        List<String> authoritiesName = authority.stream().map(Authority::getName)
+            .collect(Collectors.toList());
+
+        when(userRepo.findByEmail("taras@gmail.com")).thenReturn(Optional.of(employee));
+        when(authorityRepo.findAuthoritiesByNames(authoritiesName)).thenReturn(authority);
+        employee.setAuthorities(authority);
+        authorityService.updateEmployeesAuthorities(getUserEmployeeAuthorityDto());
+
+        verify(userRepo).save(employee);
+    }
+
+    @Test
+    void updateEmployeesAuthoritiesTestBadRequestException() {
+        User employee = createEmployee();
+        employee.setRole(Role.ROLE_USER);
+        
+        when(userRepo.findByEmail("taras@gmail.com")).thenReturn(Optional.of(employee));
+
+        assertEquals(Role.ROLE_USER, employee.getRole());
+        assertThrows(BadRequestException.class,
+            () -> authorityService.updateEmployeesAuthorities(getUserEmployeeAuthorityDto()));
+    }
 
     @Test
     void updateAuthoritiesTest() {
@@ -85,33 +100,4 @@ class AuthorityServiceImplTest {
         var dto = new UpdateEmployeeAuthoritiesDto();
         assertThrows(UsernameNotFoundException.class, () -> authorityService.updateAuthorities(dto));
     }
-
-//    @Test
-//    void updateEmployeesAuthoritiesNotFoundExceptionTest() {
-//        var dto = UserEmployeeAuthorityDto.builder()
-//            .employeeEmail("email@mail.com")
-//            .authorities(List.of("test"))
-//            .build();
-//        User employee = createEmployee();
-//        when(userRepo.findByEmail("test@mail.com")).thenReturn(Optional.of(employee));
-//        when(userRepo.findByEmail(dto.getEmployeeEmail())).thenReturn(Optional.empty());
-//        assertThrows(NotFoundException.class,
-//            () -> authorityService.updateEmployeesAuthorities(dto));
-//        verify(userRepo).findByEmail("test@mail.com");
-//    }
-//
-//    @Test
-//    void updateEmployeesAuthoritiesBadRequestExceptionTest() {
-//        User user = createUbsAdmin();
-//        User employee = createEmployee();
-//        when(userRepo.findByEmail("email@mail.com")).thenReturn(Optional.of(user));
-//        when(userRepo.findByEmail(employee.getEmail())).thenReturn(Optional.empty());
-//        var dto = UserEmployeeAuthorityDto.builder()
-//            .employeeEmail("email@mail.com")
-//            .authorities(List.of("test"))
-//            .build();
-//        assertThrows(BadRequestException.class,
-//            () -> authorityService.updateEmployeesAuthorities(dto));
-//        verify(userRepo, times(2)).findByEmail("email@mail.com");
-//    }
 }
