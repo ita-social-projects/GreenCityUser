47f97669189990d29efc18ee47a6a97a48add8cf
Oleh Kulbaba <elatedexorcist@gmail.com>
2023-02-28 17:32:41 +0200

    Tests fixed

--- service/src/test/java/greencity/security/service/VerifyEmailServiceImplTest.java
+++ service/src/test/java/greencity/security/service/VerifyEmailServiceImplTest.java
@@ -1,8 +1,11 @@
 package greencity.security.service;
 
 import greencity.client.RestClient;
+import greencity.entity.User;
 import greencity.entity.VerifyEmail;
+import greencity.enums.UserStatus;
 import greencity.exception.exceptions.UserActivationEmailTokenExpiredException;
+import greencity.repository.UserRepo;
 import greencity.security.repository.VerifyEmailRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,6 +25,14 @@ class VerifyEmailServiceImplTest {
     private VerifyEmailRepo verifyEmailRepo;
     @Mock
     RestClient restClient;
+    @Mock
+    private UserRepo userRepo;
+
+    private final User user = User.builder()
+        .id(1L)
+        .userStatus(UserStatus.CREATED)
+        .name("user")
+        .build();
 
     @InjectMocks
     private VerifyEmailServiceImpl verifyEmailService;
@@ -31,6 +42,8 @@ class VerifyEmailServiceImplTest {
         VerifyEmail verifyEmail = new VerifyEmail();
         verifyEmail.setExpiryDate(LocalDateTime.MAX);
         when(verifyEmailRepo.findByTokenAndUserId(1L, "token")).thenReturn(Optional.of(verifyEmail));
+        when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+        when(userRepo.save(any(User.class))).thenReturn(user);
         doNothing().when(restClient).addUserToSystemChat(1L);
         verifyEmailService.verifyByToken(1L, "token");
         verify(verifyEmailRepo).deleteVerifyEmailByTokenAndUserId(1L, "token");
@@ -40,6 +53,7 @@ class VerifyEmailServiceImplTest {
     void verifyByTokenExpiredTokenTest() {
         VerifyEmail verifyEmail = new VerifyEmail();
         verifyEmail.setExpiryDate(LocalDateTime.MIN);
+        when(userRepo.findById(1L)).thenReturn(Optional.of(user));
         when(verifyEmailRepo.findByTokenAndUserId(1L, "token")).thenReturn(Optional.of(verifyEmail));
         Assertions.assertThrows(UserActivationEmailTokenExpiredException.class,
             () -> verifyEmailService.verifyByToken(1L, "token"));
