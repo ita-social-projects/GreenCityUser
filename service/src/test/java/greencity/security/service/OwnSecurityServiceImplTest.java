9261a66437e502967b10fe9ee838282b4e6a9da5
JoseCaste <70968629+JoseCaste@users.noreply.github.com>
2023-03-09 19:10:55 +0000

    Deploying to gh-pages from  @ 71ce1edb2c6cd50abde2dbb13b87a0ce4b4e914e 🚀

--- service/src/test/java/greencity/security/service/OwnSecurityServiceImplTest.java
+++ service/src/test/java/greencity/security/service/OwnSecurityServiceImplTest.java
@@ -86,9 +86,6 @@ class OwnSecurityServiceImplTest {
     @Mock
     RestClient restClient;
 
-    @Mock
-    private AuthorityRepo authorityRepo;
-
     private OwnSecurityService ownSecurityService;
 
     private UserVO verifiedUser;
@@ -102,7 +99,7 @@ class OwnSecurityServiceImplTest {
         initMocks(this);
         ownSecurityService = new OwnSecurityServiceImpl(ownSecurityRepo, userService, passwordEncoder,
             jwtTool, 1, restorePasswordEmailRepo, modelMapper,
-            userRepo, achievementService, emailService, restClient, authorityRepo);
+            userRepo, achievementService, emailService, restClient);
 
         verifiedUser = UserVO.builder()
             .email("test@gmail.com")
@@ -182,6 +179,32 @@ class OwnSecurityServiceImplTest {
         verify(achievementService, times(2)).findAll();
         verify(modelMapper, times(2)).map(any(), any());
         verify(userRepo).save(any());
+        verify(jwtTool, times(2)).generateTokenKey();
+    }
+
+    @Test
+    void signUpWithDuplicatedEmployee() {
+        User user = ModelUtils.getUserWithUbsRole();
+        UserVO userVO = ModelUtils.getUserVO();
+        EmployeeSignUpDto employeeSignUpDto = ModelUtils.getEmployeeSignUpDto();
+        OwnSignUpDto ownSignUpDto = ModelUtils.getOwnSignUpDto();
+        List<Achievement> achievementList = Collections.singletonList(ModelUtils.getAchievement());
+        List<AchievementVO> achievementVOList = Collections.singletonList(ModelUtils.getAchievementVO());
+        List<UserAchievement> userAchievementList = Collections.singletonList(ModelUtils.getUserAchievement());
+        user.setUserAchievements(userAchievementList);
+        when(achievementService.findAll()).thenReturn(achievementVOList);
+
+        when(modelMapper.map(achievementVOList, new TypeToken<List<Achievement>>() {
+        }.getType())).thenReturn(achievementList);
+        when(modelMapper.map(any(User.class), eq(UserVO.class))).thenReturn(userVO);
+        when(modelMapper.map(any(EmployeeSignUpDto.class), eq(OwnSignUpDto.class))).thenReturn(ownSignUpDto);
+
+        when(userRepo.save(any(User.class))).thenThrow(DataIntegrityViolationException.class);
+        when(jwtTool.generateTokenKey()).thenReturn("New-token-key");
+
+        assertThrows(UserAlreadyRegisteredException.class,
+            () -> ownSecurityService.signUpEmployee(employeeSignUpDto, "en"));
+
         verify(jwtTool, times(2)).generateTokenKey();
     }
 
