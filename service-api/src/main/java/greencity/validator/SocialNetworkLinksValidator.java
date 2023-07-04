d118e0c4b3c691b6d88c7ebed3633a4bd7955dc3
Nazar <bokalo.n@nltu.lviv.ua>
2023-07-04 14:32:43 +0300

    edit saveUserProfile

--- service-api/src/main/java/greencity/validator/SocialNetworkLinksValidator.java
+++ service-api/src/main/java/greencity/validator/SocialNetworkLinksValidator.java
@@ -19,6 +19,9 @@ public class SocialNetworkLinksValidator implements ConstraintValidator<ValidSoc
 
     @Override
     public boolean isValid(List<String> links, ConstraintValidatorContext context) {
+        if (links == null) {
+            return true;
+        }
         if (links.size() > ValidationConstants.MAX_AMOUNT_OF_SOCIAL_NETWORK_LINKS) {
             throw new BadSocialNetworkLinksException(ErrorMessage.USER_CANNOT_ADD_MORE_THAN_5_SOCIAL_NETWORK_LINKS);
         }
