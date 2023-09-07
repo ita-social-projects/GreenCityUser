fa6ee778da1a56c473c4d0b5f6592ce6e72797dc
ospodaryk <ospodaryk11@gmail.com>
2023-09-07 13:06:31 +0300

    Fixed test

--- service-api/src/main/java/greencity/dto/user/UserRatingDto.java
+++ service-api/src/main/java/greencity/dto/user/UserRatingDto.java
@@ -1,11 +1,12 @@
 package greencity.dto.user;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class UserRatingDto {
@@ -14,25 +15,4 @@ public class UserRatingDto {
     private String email;
 
     private Double rating;
-
-    /**
-     * Constructs a new UserRatingDto object with the specified values.
-     *
-     * @param id     The unique identifier of the user rating.
-     * @param email  The email address associated with the user rating.
-     * @param rating The rating value.
-     *
-     * @JsonCreator and @JsonProperty annotations are used to indicate this
-     *              constructor should be used for JSON deserialization, and to map
-     *              the JSON properties to constructor parameters respectively.
-     */
-    @JsonCreator
-    public UserRatingDto(
-        @JsonProperty("id") Long id,
-        @JsonProperty("email") String email,
-        @JsonProperty("rating") Double rating) {
-        this.id = id;
-        this.email = email;
-        this.rating = rating;
-    }
 }
