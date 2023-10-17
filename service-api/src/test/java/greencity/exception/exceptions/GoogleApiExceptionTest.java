a45e26ec9795c2b6abc758049c3ef622d193717b
Yurii Midianyi <loopfordev@gmail.com>
2023-10-17 19:33:55 +0300

    fix code smell

--- service-api/src/test/java/greencity/exception/exceptions/GoogleApiExceptionTest.java
+++ service-api/src/test/java/greencity/exception/exceptions/GoogleApiExceptionTest.java
@@ -7,7 +7,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
-public class GoogleApiExceptionTest {
+class GoogleApiExceptionTest {
     @Test
     void testGoogleApiExceptionMessage() {
         String message = "Test exception message";
