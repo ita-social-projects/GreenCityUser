7c9e1f9e2b215a37cb7a4e494b95c5b19a578807
VinuVicho <vovkamlad@gmail.com>
2023-11-05 22:29:53 +0200

    Exeption Test Created

---
+++ service-api/src/test/java/greencity/exception/exceptions/LanguageNotFoundExceptionTest.java
@@ -0,0 +1,46 @@
+package greencity.exception.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class LanguageNotFoundExceptionTest {
+
+    @Test
+    public void testDefaultConstructor() {
+        // Arrange
+        LanguageNotFoundException exception = new LanguageNotFoundException();
+
+        // Act & Assert
+        assertEquals(null, exception.getMessage());
+    }
+
+    @Test
+    public void testMessageConstructor() {
+        // Arrange
+        String message = "Custom exception message";
+        LanguageNotFoundException exception = new LanguageNotFoundException(message);
+
+        // Act & Assert
+        assertEquals(message, exception.getMessage());
+    }
+
+    @Test
+    public void testMessageConstructorWithMockito() {
+        // Arrange
+        String message = "Custom exception message";
+        LanguageNotFoundException exception = Mockito.spy(new LanguageNotFoundException(message));
+
+        // Act
+        String actualMessage = exception.getMessage();
+
+        // Assert
+        assertEquals(message, actualMessage);
+        verify(exception).getMessage(); // Verify that getMessage() was called
+    }
+}
