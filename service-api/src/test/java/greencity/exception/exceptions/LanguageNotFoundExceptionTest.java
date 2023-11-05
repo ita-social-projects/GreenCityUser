119fb48a6af1d37306ef03d31f81c81e3c9129d9
VinuVicho <vovkamlad@gmail.com>
2023-11-06 00:25:46 +0200

    Code cleanup

--- service-api/src/test/java/greencity/exception/exceptions/LanguageNotFoundExceptionTest.java
+++ service-api/src/test/java/greencity/exception/exceptions/LanguageNotFoundExceptionTest.java
@@ -12,33 +12,22 @@ import static org.mockito.Mockito.verify;
 class LanguageNotFoundExceptionTest {
     @Test
     void testDefaultConstructor() {
-        // Arrange
         LanguageNotFoundException exception = new LanguageNotFoundException();
-
-        // Act & Assert
         assertEquals(null, exception.getMessage());
     }
 
     @Test
     void testMessageConstructor() {
-        // Arrange
         String message = "Custom exception message";
         LanguageNotFoundException exception = new LanguageNotFoundException(message);
-
-        // Act & Assert
         assertEquals(message, exception.getMessage());
     }
 
     @Test
     void testMessageConstructorWithMockito() {
-        // Arrange
         String message = "Custom exception message";
         LanguageNotFoundException exception = Mockito.spy(new LanguageNotFoundException(message));
-
-        // Act
         String actualMessage = exception.getMessage();
-
-        // Assert
         assertEquals(message, actualMessage);
         verify(exception).getMessage(); // Verify that getMessage() was called
     }
