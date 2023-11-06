5085c9a05d3d7c737664a0670072000a12642219
Yurii Midianyi <loopfordev@gmail.com>
2023-11-06 10:42:39 +0200

    used data annotation to decrease annotations used

--- service-api/src/main/java/greencity/dto/CoordinatesDto.java
+++ service-api/src/main/java/greencity/dto/CoordinatesDto.java
@@ -2,15 +2,13 @@ package greencity.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 @EqualsAndHashCode
 @Builder
 public class CoordinatesDto {
