a3b7df6dc908cd9c4f93339efb7d02688ed48012
Yurii Midianyi <loopfordev@gmail.com>
2023-11-06 11:04:39 +0200

    deleted redundant annotation equals and hashcode

--- service-api/src/main/java/greencity/dto/CoordinatesDto.java
+++ service-api/src/main/java/greencity/dto/CoordinatesDto.java
@@ -3,13 +3,11 @@ package greencity.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@EqualsAndHashCode
 @Builder
 public class CoordinatesDto {
     private Double latitude;
