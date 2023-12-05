9f3c26e82e63edb8c36f6fc0e73d4d165f689d11
VinuVicho <vovkamlad@gmail.com>
2023-12-05 15:02:20 +0200

    Code refactored

--- service-api/src/main/java/greencity/validator/EmailAddressValidator.java
+++ service-api/src/main/java/greencity/validator/EmailAddressValidator.java
@@ -5,7 +5,6 @@ import greencity.exception.exceptions.WrongEmailException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -23,12 +22,8 @@ public class EmailAddressValidator {
      * @param emailAddress to be validated
      */
     public static void validate(String emailAddress) {
-        if (emailAddress == null) {
-            throw new WrongEmailException("Email address cannot be null");
-        }
-        Matcher m = REGEX_PATTERN.matcher(emailAddress);
-        if (!m.matches()) {
-            throw new WrongEmailException("Invalid email address " + emailAddress);
+        if (emailAddress == null || !REGEX_PATTERN.matcher(emailAddress).matches()) {
+            throw new WrongEmailException("Invalid email address: " + emailAddress);
         }
     }
 }
