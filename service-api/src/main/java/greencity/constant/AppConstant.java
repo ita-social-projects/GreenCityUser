16e1f26a3bcbcac1b93cf495804ed7a81e27ac10
VinuVicho <vovkamlad@gmail.com>
2023-12-05 10:48:45 +0200

    Updated email regex

--- service-api/src/main/java/greencity/constant/AppConstant.java
+++ service-api/src/main/java/greencity/constant/AppConstant.java
@@ -10,7 +10,8 @@ public final class AppConstant {
     public static final String UBS_EMPLOYEE = "UBS_EMPLOYEE";
     public static final String ROLE = "role";
     public static final String AUTHORIZATION = "Authorization";
-    public static final String VALIDATION_EMAIL = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    public static final String VALIDATION_EMAIL =
+        "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     public static final Double DEFAULT_RATING = 0.0;
     public static final String USERNAME = "name";
     public static final String FACEBOOK_OBJECT_ID = "me";
