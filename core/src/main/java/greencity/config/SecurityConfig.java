b75aa754374c63f4f6be075ae8461ce94d625134
Anton Bondar <bondar.local@gmail.com>
2024-03-18 15:03:16 +0200

    cors configuration V13

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import greencity.service.UserService;
 import static jakarta.servlet.http.HttpServletResponse.SC_FORBIDDEN;
 import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -246,19 +247,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(
-            List.of("http://localhost:4205",
-                "http://localhost:4205/",
-                "http://localhost:4205/*",
-                "http://localhost:4200",
-                "http://localhost:4200/",
-                "http://localhost:4200/*",
-                "http://localhost:8085",
-                "http://localhost:8085/",
-                "http://localhost:8085/*",
-                "https://greencity.greencity.social",
-                "https://greencity.greencity.social/",
-                "https://greencity.greencity.social/*"));
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
         configuration.setAllowedMethods(
             Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
         configuration.setAllowedHeaders(
