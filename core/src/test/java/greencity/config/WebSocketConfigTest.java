a98b7e5ae6967c7a75fdeeeaee0d6334d735948c
Anton Bondar <bondar.local@gmail.com>
2024-03-27 23:09:12 +0200

    tests

--- core/src/test/java/greencity/config/WebSocketConfigTest.java
+++ core/src/test/java/greencity/config/WebSocketConfigTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 
-public class WebSocketConfigTest {
+class WebSocketConfigTest {
     private WebSocketConfig webSocketConfig;
 
     @BeforeEach
