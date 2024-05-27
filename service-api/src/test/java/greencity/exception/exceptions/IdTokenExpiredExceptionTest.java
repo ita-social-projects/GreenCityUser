8fd86c52075b3b4c801ae40a6a58bfe568a26238
Anton Bondar <bondar.local@gmail.com>
2024-05-27 14:44:15 +0300

    tests

--- service-api/src/test/java/greencity/exception/exceptions/IdTokenExpiredExceptionTest.java
+++ service-api/src/test/java/greencity/exception/exceptions/IdTokenExpiredExceptionTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class IdTokenExpiredExceptionTest {
     @Test
     void idTokenExpiredExceptionMessageTest() {
-        String message = "Id token is not valid or expired. ";
+        String message = "Google id token is not valid or expired. ";
         var exception = new IdTokenExpiredException(message);
         Assertions.assertEquals(ErrorMessage.EXPIRED_GOOGLE_ID_TOKEN, exception.getMessage());
     }
