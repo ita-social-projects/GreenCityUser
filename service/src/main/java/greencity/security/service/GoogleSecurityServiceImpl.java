ab8c3f0c33d061ae34382c9ad76ef16e51193280
ABbondar <53192419+ABbondar@users.noreply.github.com>
2024-05-27 12:28:37 +0000

    Deploying to gh-pages from  @ 18698568c468721a77e6e866b4a2e54f0c863646 🚀

--- service/src/main/java/greencity/security/service/GoogleSecurityServiceImpl.java
+++ service/src/main/java/greencity/security/service/GoogleSecurityServiceImpl.java
@@ -1,13 +1,13 @@
 package greencity.security.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import greencity.client.RestClient;
 import static greencity.constant.AppConstant.DEFAULT_RATING;
-import static greencity.constant.AppConstant.GOOGLE_PICTURE;
-import static greencity.constant.AppConstant.USERNAME;
 import greencity.constant.ErrorMessage;
 import greencity.dto.ubs.UbsProfileCreationDto;
+import greencity.dto.user.UserInfo;
 import greencity.dto.user.UserVO;
 import greencity.entity.Language;
 import greencity.entity.User;
@@ -16,10 +16,13 @@ import greencity.entity.UserAction;
 import greencity.enums.EmailNotification;
 import greencity.enums.Role;
 import greencity.enums.UserStatus;
+import greencity.exception.exceptions.IdTokenExpiredException;
 import greencity.exception.exceptions.UserDeactivatedException;
 import greencity.repository.UserRepo;
 import greencity.security.dto.SuccessSignInDto;
 import greencity.security.jwt.JwtTool;
+import static greencity.constant.AppConstant.GOOGLE_PICTURE;
+import static greencity.constant.AppConstant.USERNAME;
 import static greencity.security.service.OwnSecurityServiceImpl.getUserAchievements;
 import static greencity.security.service.OwnSecurityServiceImpl.getUserActions;
 import greencity.service.AchievementService;
@@ -30,11 +33,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.web.client.RestClientException;
 
 /**
  * {@inheritDoc}
@@ -50,18 +59,29 @@ public class GoogleSecurityServiceImpl implements GoogleSecurityService {
     private final UserRepo userRepo;
     private final RestClient restClient;
     private final PlatformTransactionManager transactionManager;
+    private final HttpClient googleAccessTokenVerifier;
+    private final ObjectMapper objectMapper;
+
+    @Value("${google.resource.userInfoUri}")
+    private String userInfoUrl;
 
     /**
      * Constructor.
      *
-     * @param userService           {@link UserService} - service of {@link User}
-     *                              logic.
-     * @param jwtTool               {@link JwtTool} - tool for jwt logic.
-     * @param googleIdTokenVerifier {@link GoogleIdTokenVerifier} - tool for verify.
-     * @param modelMapper           {@link ModelMapper} - tool for mapping models.
-     * @param restClient            {@link RestClient} - tool for sending requests
-     * @param transactionManager    {@link PlatformTransactionManager} - tool for
-     *                              transaction management
+     * @param userService               {@link UserService} - service of
+     *                                  {@link User} logic.
+     * @param jwtTool                   {@link JwtTool} - tool for jwt logic.
+     * @param googleIdTokenVerifier     {@link GoogleIdTokenVerifier} - tool for
+     *                                  verifying Google Id Token.
+     * @param modelMapper               {@link ModelMapper} - tool for mapping
+     *                                  models.
+     * @param restClient                {@link RestClient} - tool for sending
+     *                                  requests
+     * @param transactionManager        {@link PlatformTransactionManager} - tool
+     *                                  for transaction management
+     * @param googleAccessTokenVerifier {@link HttpClient} - client for HTTP request
+     *                                  execution for verifying Google Access Token.
+     * @param objectMapper              {@link ObjectMapper} - object mapper.
      */
     @Autowired
     public GoogleSecurityServiceImpl(UserService userService,
@@ -71,7 +91,9 @@ public class GoogleSecurityServiceImpl implements GoogleSecurityService {
         AchievementService achievementService,
         UserRepo userRepo,
         RestClient restClient,
-        PlatformTransactionManager transactionManager) {
+        PlatformTransactionManager transactionManager,
+        HttpClient googleAccessTokenVerifier,
+        ObjectMapper objectMapper) {
         this.userService = userService;
         this.jwtTool = jwtTool;
         this.googleIdTokenVerifier = googleIdTokenVerifier;
@@ -80,55 +102,76 @@ public class GoogleSecurityServiceImpl implements GoogleSecurityService {
         this.userRepo = userRepo;
         this.restClient = restClient;
         this.transactionManager = transactionManager;
+        this.googleAccessTokenVerifier = googleAccessTokenVerifier;
+        this.objectMapper = objectMapper;
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public SuccessSignInDto authenticate(String idToken, String language) {
+    public SuccessSignInDto authenticate(String googleToken, String language) {
         try {
-            GoogleIdToken googleIdToken = googleIdTokenVerifier.verify(idToken);
-            if (googleIdToken != null) {
-                GoogleIdToken.Payload payload = googleIdToken.getPayload();
-                String email = payload.getEmail();
-                String userName = (String) payload.get(USERNAME);
-                UserVO userVO = userService.findByEmail(email);
-                if (userVO == null) {
-                    log.info(ErrorMessage.USER_NOT_FOUND_BY_EMAIL + email);
-                    String profilePicture = (String) payload.get(GOOGLE_PICTURE);
-                    TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
-                    User user = transactionTemplate.execute(status -> {
-                        User savedUser = createNewUser(email, userName, profilePicture, language);
-                        savedUser.setUserAchievements(createUserAchievements(savedUser));
-                        savedUser.setUserActions(createUserActions(savedUser));
-                        savedUser.setUuid(UUID.randomUUID().toString());
-                        savedUser.setFirstName(userName);
-                        Long id = userRepo.save(savedUser).getId();
-                        savedUser.setId(id);
-                        return savedUser;
-                    });
-                    restClient.createUbsProfile(modelMapper.map(user, UbsProfileCreationDto.class));
-                    userVO = modelMapper.map(user, UserVO.class);
-                    log.info("Google sign-up and sign-in user - {}", userVO.getEmail());
-                    return getSuccessSignInDto(userVO);
-                } else {
-                    if (userVO.getUserStatus() == UserStatus.DEACTIVATED) {
-                        throw new UserDeactivatedException(ErrorMessage.USER_DEACTIVATED);
-                    }
-                    log.info("Google sign-in exist user - {}", userVO.getEmail());
-                    return getSuccessSignInDto(userVO);
-                }
-            } else {
-                throw new IllegalArgumentException(ErrorMessage.BAD_GOOGLE_TOKEN);
+            GoogleIdToken googleIdToken = googleIdTokenVerifier.verify(googleToken);
+            if (googleIdToken == null) {
+                throw new IdTokenExpiredException(ErrorMessage.EXPIRED_GOOGLE_ID_TOKEN);
             }
+            String email = googleIdToken.getPayload().getEmail();
+            String userName = (String) googleIdToken.getPayload().get(USERNAME);
+            String profilePicture = (String) googleIdToken.getPayload().get(GOOGLE_PICTURE);
+            return processAuthentication(email, userName, profilePicture, language);
+        } catch (IllegalArgumentException e) {
+            return authenticateByGoogleAccessToken(googleToken, language);
         } catch (GeneralSecurityException | IOException e) {
-            throw new IllegalArgumentException(ErrorMessage.BAD_GOOGLE_TOKEN + ". " + e.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.BAD_GOOGLE_TOKEN + e.getMessage());
         }
     }
 
-    private User createNewUser(String email, String userName, String profilePicture,
+    private SuccessSignInDto authenticateByGoogleAccessToken(String googleAccessToken, String language) {
+        try {
+            UserInfo userInfo = getUserInfoFromGoogleAccessToken(googleAccessToken);
+            if (userInfo.getEmail() == null) {
+                throw new IllegalArgumentException(ErrorMessage.BAD_GOOGLE_TOKEN);
+            }
+            String email = userInfo.getEmail();
+            String userName = userInfo.getName();
+            String profilePicture = userInfo.getPicture();
+            return processAuthentication(email, userName, profilePicture, language);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(ErrorMessage.BAD_GOOGLE_TOKEN + e.getMessage());
+        }
+    }
+
+    private SuccessSignInDto processAuthentication(String email, String userName, String profilePicture,
         String language) {
+        UserVO userVO = userService.findByEmail(email);
+        if (userVO == null) {
+            log.info(ErrorMessage.USER_NOT_FOUND_BY_EMAIL + email);
+            return handleNewUser(email, userName, profilePicture, language);
+        } else {
+            if (userVO.getUserStatus() == UserStatus.DEACTIVATED) {
+                throw new UserDeactivatedException(ErrorMessage.USER_DEACTIVATED);
+            }
+            log.info("Google sign-in exist user - {}", userVO.getEmail());
+            return getSuccessSignInDto(userVO);
+        }
+    }
+
+    private SuccessSignInDto handleNewUser(String email, String userName, String profilePicture, String language) {
+        User newUser = createNewUser(email, userName, profilePicture, language);
+        User savedUser = saveNewUser(newUser);
+        try {
+            restClient.createUbsProfile(modelMapper.map(savedUser, UbsProfileCreationDto.class));
+        } catch (RestClientException e) {
+            log.error("Failed to create UBS profile for user - {}", savedUser.getEmail(), e);
+            throw new RestClientException(ErrorMessage.TRANSACTION_FAILED, e);
+        }
+        UserVO userVO = modelMapper.map(savedUser, UserVO.class);
+        log.info("Google sign-up and sign-in user - {}", userVO.getEmail());
+        return getSuccessSignInDto(userVO);
+    }
+
+    private User createNewUser(String email, String userName, String profilePicture, String language) {
         return User.builder()
             .email(email)
             .name(userName)
@@ -140,10 +183,20 @@ public class GoogleSecurityServiceImpl implements GoogleSecurityService {
             .refreshTokenKey(jwtTool.generateTokenKey())
             .profilePicturePath(profilePicture)
             .rating(DEFAULT_RATING)
-            .language(Language.builder()
-                .id(modelMapper.map(language, Long.class))
-                .build())
+            .language(Language.builder().id(modelMapper.map(language, Long.class)).build())
             .build();
+    }
+
+    private User saveNewUser(User newUser) {
+        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+        return transactionTemplate.execute(status -> {
+            newUser.setUserAchievements(createUserAchievements(newUser));
+            newUser.setUserActions(createUserActions(newUser));
+            newUser.setUuid(UUID.randomUUID().toString());
+            Long id = userRepo.save(newUser).getId();
+            newUser.setId(id);
+            return newUser;
+        });
     }
 
     private List<UserAchievement> createUserAchievements(User user) {
@@ -158,5 +211,13 @@ public class GoogleSecurityServiceImpl implements GoogleSecurityService {
         String accessToken = jwtTool.createAccessToken(user.getEmail(), user.getRole());
         String refreshToken = jwtTool.createRefreshToken(user);
         return new SuccessSignInDto(user.getId(), accessToken, refreshToken, user.getName(), false);
+    }
+
+    private UserInfo getUserInfoFromGoogleAccessToken(String accessToken) throws IOException {
+        String requestUrl = userInfoUrl + accessToken;
+        HttpGet request = new HttpGet(requestUrl);
+        HttpResponse response = googleAccessTokenVerifier.execute(request);
+        String jsonResponse = EntityUtils.toString(response.getEntity());
+        return objectMapper.readValue(jsonResponse, UserInfo.class);
     }
 }
