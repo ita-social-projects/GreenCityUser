2c59d34c7190672cf83482df06fdeacd9603aa39
IliaWithHat <rozhkoilia5533@gmail.com>
2024-08-07 11:45:59 +0300

    SonarCloud fix

--- dao/src/main/java/greencity/constant/CacheConstants.java
+++ dao/src/main/java/greencity/constant/CacheConstants.java
@@ -1,11 +1,11 @@
 package greencity.constant;
 
-import lombok.RequiredArgsConstructor;
+import lombok.experimental.UtilityClass;
 
 /**
  * Class, that contains constants used for caching.
  */
-@RequiredArgsConstructor
+@UtilityClass
 public class CacheConstants {
     public static final String ALL_ACHIEVEMENTS_CACHE_NAME = "achievements_cache";
 }
