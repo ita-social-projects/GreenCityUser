ef261e77998bbe2268c15de50e20e230be34b0cf
Yehor <egorov2001egorr@gmail.com>
2024-08-09 09:50:07 +0300

    3015-Delete-user-profile-and-change-password - Refactor import statements in OwnSecurityController.
    Separated import statements for clarity and organization. This change improves readability and maintainability of both the controller and its test file.

--- core/src/test/java/greencity/security/controller/OwnSecurityControllerTest.java
+++ core/src/test/java/greencity/security/controller/OwnSecurityControllerTest.java
@@ -24,6 +24,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
