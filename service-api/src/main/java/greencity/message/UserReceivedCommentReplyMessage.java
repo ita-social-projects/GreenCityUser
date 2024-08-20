f818c904c53e3486d91b12ed3cc539e0808e124c
DmytroDmytruk <97033032+DmytroDmytruk@users.noreply.github.com>
2024-08-20 23:33:41 +0300

    Resolved code duplication

--- service-api/src/main/java/greencity/message/UserReceivedCommentReplyMessage.java
+++ service-api/src/main/java/greencity/message/UserReceivedCommentReplyMessage.java
@@ -2,41 +2,17 @@ package greencity.message;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class UserReceivedCommentReplyMessage implements Serializable {
+@Getter
+public class UserReceivedCommentReplyMessage extends CommentMessage implements Serializable {
     @NotEmpty(message = "Author name cannot be empty")
     private String authorName;
-
-    @NotEmpty(message = "Receiver name cannot be empty")
-    private String receiverName;
-
-    @NotEmpty(message = "Receiver email cannot be empty")
-    private String receiverEmail;
-
-    @NotEmpty(message = "Language cannot be empty")
-    private String language;
-
-    @NotEmpty(message = "Name of element cannot be null")
-    private String elementName;
-
-    @NotEmpty(message = "Comment cannot be empty")
-    private String commentText;
-
-    @NotNull(message = "Date cannot be null")
-    private LocalDateTime creationDate;
-
-    @NotNull(message = "Element ID cannot be null")
-    private Long commentedElementId;
 
     @NotEmpty(message = "Parent comment cannot be empty")
     private String parentCommentText;
@@ -47,6 +23,16 @@ public class UserReceivedCommentReplyMessage implements Serializable {
     @NotNull(message = "Parent comment creation date cannot be null")
     private LocalDateTime parentCommentCreationDate;
 
-    @NotEmpty(message = "Base link cannot be null")
-    private String baseLink;
+    @Builder
+    public UserReceivedCommentReplyMessage(String receiverName, String receiverEmail, String language,
+        String elementName, String commentText, LocalDateTime creationDate,
+        Long commentedElementId, String baseLink, String authorName, String parentCommentText,
+        String parentCommentAuthorName, LocalDateTime parentCommentCreationDate) {
+        super(receiverName, receiverEmail, language, elementName, commentText, creationDate, commentedElementId,
+            baseLink);
+        this.authorName = authorName;
+        this.parentCommentText = parentCommentText;
+        this.parentCommentAuthorName = parentCommentAuthorName;
+        this.parentCommentCreationDate = parentCommentCreationDate;
+    }
 }
