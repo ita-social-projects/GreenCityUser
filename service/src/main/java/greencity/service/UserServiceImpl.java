3af93c948a618e0bfdfefcc7f06f3b000d4baecf
Viktoriia Herchanivska <viktoriia.herchanivska@gmail.com>
2024-09-17 16:33:17 +0300

    remove unused import

--- service/src/main/java/greencity/service/UserServiceImpl.java
+++ service/src/main/java/greencity/service/UserServiceImpl.java
@@ -83,7 +83,6 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
