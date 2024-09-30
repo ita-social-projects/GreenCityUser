c6681dad35c4f460d7a9f98fef4fe659f8e992d7
K4sik <kasiks597@gmail.com>
2024-09-30 18:13:17 +0300

    Fixed Sonar Issue

--- service-api/src/test/java/greencity/filters/SearchCriteriaTest.java
+++ service-api/src/test/java/greencity/filters/SearchCriteriaTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
