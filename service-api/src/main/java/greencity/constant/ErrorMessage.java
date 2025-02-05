9a87f6c3eea78b9bc4519da43e0693f6ba4bc02f
VasylyshynDmytro <147658570+VasylyshynDmytro@users.noreply.github.com>
2025-02-05 12:12:46 +0200

    Update ErrorMessage.java

--- service-api/src/main/java/greencity/constant/ErrorMessage.java
+++ service-api/src/main/java/greencity/constant/ErrorMessage.java
@@ -64,5 +64,4 @@ public class ErrorMessage {
     public static final String GIT_REPOSITORY_NOT_INITIALIZED =
         "Git repository not initialized. Commit info is unavailable.";
     public static final String FAILED_TO_FETCH_COMMIT_INFO = "Failed to fetch commit info due to I/O error: ";
-
 }
