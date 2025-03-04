7e2849d13d151d4c52ff7cdde6ddc81a390ab415
Maksym Kozak <bersercer70@gmail.com>
2025-03-04 20:57:13 +0200

    test commit

--- core/src/main/java/greencity/UserApplication.java
+++ core/src/main/java/greencity/UserApplication.java
@@ -14,5 +14,6 @@ public class UserApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+        System.out.println("Hello world");
     }
 }
