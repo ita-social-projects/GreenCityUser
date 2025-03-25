73348e19be78e4ca67750ffc7b4f354d5c8d5efe
Nazar <nazikvaver@gmail.com>
2025-03-25 17:50:48 +0200

    Removed unused imports

--- core/src/test/java/greencity/security/controller/FacebookSecurityControllerTest.java
+++ core/src/test/java/greencity/security/controller/FacebookSecurityControllerTest.java
@@ -1,6 +1,5 @@
 package greencity.security.controller;
 
-import greencity.security.dto.SuccessSignInDto;
 import greencity.security.service.FacebookSecurityService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,19 +7,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ExtendWith(MockitoExtension.class)
