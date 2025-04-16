6ffba65a41253dd9de349f684c8e35244a114278
Maksym Kozak <bersercer70@gmail.com>
2025-04-16 12:31:45 +0300

    update regexp for name field

--- service-api/src/main/java/greencity/constant/ValidationConstants.java
+++ service-api/src/main/java/greencity/constant/ValidationConstants.java
@@ -6,16 +6,12 @@ import lombok.experimental.UtilityClass;
 public class ValidationConstants {
     public static final String EMAIL_REGEXP = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
     public static final String INVALID_EMAIL = "{greenCity.validation.invalid.email}";
-    public static final String USERNAME_MESSAGE =
-        """
-            The name ${validatedValue} cannot be empty, \
-            starts with a number or not a capital letter, \
-            ends with dot, \
-            contain 2 consecutive dots or dashes and symbols like @#$. \
+    public static final String USERNAME_MESSAGE = """
+            Name must start with a capital letter, \
+            cannot end with dot \
+            or contain 2 consecutive dots, dashes and special symbols. \
             Use English or Ukrainian letters, \
-            no longer than 30 symbols, \
-            the name ${validatedValue} could contain numbers, symbols '’, \
-            dot in the middle of the name, dash and whitespaces.\
+            no longer than 30 symbols.\
             """;
 
     public static final String USER_CREATED = "{greenCity.validation.user.created}";
@@ -24,6 +20,11 @@ public class ValidationConstants {
     public static final int CATEGORY_NAME_MAX_LENGTH = 30;
     public static final String CATEGORY_NAME_BAD_FORMED = "{greenCity.validation.bad.formed.category.name}";
     public static final int PLACE_NAME_MAX_LENGTH = 30;
-    public static final String USERNAME_REGEXP =
-        "^[ґҐіІєЄїЇА-Яа-яa-zA-Z](?!.*\\.$)(?!.*?\\.\\.)(?!.*?--)(?!.*?'')[-'ʼ’ ґҐіІєЄїЇА-Яа-я\\w.]{0,29}$";
+    public static final String USERNAME_REGEXP = """
+                ^(?!.*\\.\\.)(?!.*\\.$)(?!.*\\-\\-)\
+                (?=[ЄІЇҐЁА-ЯA-Z])\
+                [ЄІЇҐЁєіїґёА-Яа-яA-Za-z0-9\\s-'’.\\"]\
+                {1,30}\
+                (?<![ЭэЁёъЪЫы])$\
+                """;
 }
