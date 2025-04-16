c892d9e7152ed799728fe2c1264851cb705255d1
Maksym Kozak <bersercer70@gmail.com>
2025-04-16 12:39:38 +0300

    format

--- service-api/src/main/java/greencity/constant/ValidationConstants.java
+++ service-api/src/main/java/greencity/constant/ValidationConstants.java
@@ -7,12 +7,12 @@ public class ValidationConstants {
     public static final String EMAIL_REGEXP = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
     public static final String INVALID_EMAIL = "{greenCity.validation.invalid.email}";
     public static final String USERNAME_MESSAGE = """
-            Name must start with a capital letter, \
-            cannot end with dot \
-            or contain 2 consecutive dots, dashes and special symbols. \
-            Use English or Ukrainian letters, \
-            no longer than 30 symbols.\
-            """;
+        Name must start with a capital letter, \
+        cannot end with dot \
+        or contain 2 consecutive dots, dashes and special symbols. \
+        Use English or Ukrainian letters, \
+        no longer than 30 symbols.\
+        """;
 
     public static final String USER_CREATED = "{greenCity.validation.user.created}";
     public static final int MAX_AMOUNT_OF_SOCIAL_NETWORK_LINKS = 5;
@@ -21,10 +21,10 @@ public class ValidationConstants {
     public static final String CATEGORY_NAME_BAD_FORMED = "{greenCity.validation.bad.formed.category.name}";
     public static final int PLACE_NAME_MAX_LENGTH = 30;
     public static final String USERNAME_REGEXP = """
-                ^(?!.*\\.\\.)(?!.*\\.$)(?!.*\\-\\-)\
-                (?=[ЄІЇҐЁА-ЯA-Z])\
-                [ЄІЇҐЁєіїґёА-Яа-яA-Za-z0-9\\s-'’.\\"]\
-                {1,30}\
-                (?<![ЭэЁёъЪЫы])$\
-                """;
+        ^(?!.*\\.\\.)(?!.*\\.$)(?!.*\\-\\-)\
+        (?=[ЄІЇҐЁА-ЯA-Z])\
+        [ЄІЇҐЁєіїґёА-Яа-яA-Za-z0-9\\s-'’.\\"]\
+        {1,30}\
+        (?<![ЭэЁёъЪЫы])$\
+        """;
 }
