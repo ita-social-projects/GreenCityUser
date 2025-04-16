63a3e03d76942efd1963b68e49d0d1efd9eb3718
Maksym Kozak <bersercer70@gmail.com>
2025-04-16 23:18:16 +0300

    add method to upload files to GreenCityRemoteClient

--- service-api/src/main/java/greencity/client/GreenCityRemoteClient.java
+++ service-api/src/main/java/greencity/client/GreenCityRemoteClient.java
@@ -2,11 +2,29 @@ package greencity.client;
 
 import greencity.client.config.GreenCityRemoteClientFallbackFactory;
 import greencity.client.config.GreenCityRemoteClientInterceptor;
+import lombok.NonNull;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
 
 @FeignClient(name = "greencity-remote-client",
         url = "${greencity.server.address}",
         configuration = GreenCityRemoteClientInterceptor.class,
         fallbackFactory = GreenCityRemoteClientFallbackFactory.class)
 public interface GreenCityRemoteClient {
+
+    /**
+     * Method for uploading a files.
+     *
+     * @param files files to save.
+     * @return urls of the saved files.
+     */
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    Optional<List<String>> uploadFile(@RequestPart @NonNull List<MultipartFile> files);
+
 }
--- service-api/src/main/java/greencity/client/config/GreenCityRemoteClientFallbackFactory.java
+++ service-api/src/main/java/greencity/client/config/GreenCityRemoteClientFallbackFactory.java
@@ -2,8 +2,13 @@ package greencity.client.config;
 
 import feign.hystrix.FallbackFactory;
 import greencity.client.GreenCityRemoteClient;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @Slf4j
@@ -11,7 +16,11 @@ public class GreenCityRemoteClientFallbackFactory implements FallbackFactory<Gre
     @Override
     public GreenCityRemoteClient create(Throwable throwable) {
         return new GreenCityRemoteClient() {
-
+            @Override
+            public Optional<List<String>> uploadFile(@NonNull List<MultipartFile> files) {
+                //TODO: log
+                return Optional.empty();
+            }
         };
     }
 }
