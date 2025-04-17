f51c323607b507f1101d0084acc266e36eed5529
Maksym Kozak <bersercer70@gmail.com>
2025-04-17 11:27:41 +0300

    move 'userAchievements' field to GreenCity

--- dao/src/main/java/greencity/entity/User.java
+++ dao/src/main/java/greencity/entity/User.java
@@ -113,10 +113,6 @@ public class User {
         inverseJoinColumns = @JoinColumn(name = "friend_id", referencedColumnName = "id"))
     private List<User> userFriends = new ArrayList<>();
 
-    @Builder.Default
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<UserAchievement> userAchievements = new ArrayList<>();
-
     @Column(name = "rating")
     private Double rating;
 
