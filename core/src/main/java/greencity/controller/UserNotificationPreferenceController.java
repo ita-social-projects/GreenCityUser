2fd0f358b453b823b68e69a5346328163333809f
Maksym Kozak <bersercer70@gmail.com>
2025-04-18 18:51:02 +0300

    fix mapping

--- core/src/main/java/greencity/controller/UserNotificationPreferenceController.java
+++ core/src/main/java/greencity/controller/UserNotificationPreferenceController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -58,7 +59,7 @@ public class UserNotificationPreferenceController {
             @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
             @ApiResponse(responseCode = "403", description = HttpStatuses.FORBIDDEN)
     })
-    @GetMapping("/search")
+    @PostMapping("/search")
     public ResponseEntity<Boolean> existsByUserIdAndEmailPreferenceAndPeriodicity(@RequestBody EmailPreferenceDto emailPreferenceDto) {
         return ResponseEntity.ok().body(userNotificationPreferenceService.existsByUserIdAndEmailPreferenceAndPeriodicity(emailPreferenceDto));
     }
