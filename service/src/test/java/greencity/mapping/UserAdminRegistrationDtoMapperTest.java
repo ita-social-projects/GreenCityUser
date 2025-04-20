fd17b3eed1da9edf13a2008db24c23b4ee76e91e
Maksym Kozak <bersercer70@gmail.com>
2025-04-20 22:00:08 +0300

    fix UserAdminRegistrationDtoMapperTest

--- service/src/test/java/greencity/mapping/UserAdminRegistrationDtoMapperTest.java
+++ service/src/test/java/greencity/mapping/UserAdminRegistrationDtoMapperTest.java
@@ -1,17 +1,25 @@
 package greencity.mapping;
 
 import greencity.ModelUtils;
+import greencity.client.GreenCityRemoteClient;
+import greencity.dto.language.LanguageVO;
 import greencity.entity.User;
 import greencity.enums.UserStatus;
-import java.time.LocalDateTime;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserAdminRegistrationDtoMapperTest {
+
+    @Mock
+    GreenCityRemoteClient greenCityRemoteClient;
 
     @InjectMocks
     UserAdminRegistrationDtoMapper mapper;
@@ -20,8 +28,13 @@ class UserAdminRegistrationDtoMapperTest {
     void convert() {
         User user = ModelUtils.getUser();
         user.setUserStatus(UserStatus.BLOCKED);
-        user.setLanguageId(2L);
+        Long languageId = ModelUtils.getLanguageId();
+        LanguageVO languageVO = ModelUtils.getLanguageVO();
+        user.setLanguageId(languageId);
         user.setDateOfRegistration(LocalDateTime.of(2020, 6, 6, 13, 47));
+
+        when(greenCityRemoteClient.findLanguageById(languageId))
+                        .thenReturn(languageVO);
 
         assertEquals(ModelUtils.getUserAdminRegistrationDto(), mapper.convert(user));
     }
