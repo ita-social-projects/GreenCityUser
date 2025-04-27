e7fffad50075b09c713d65e2d0e92580bac94a01
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-04-27 22:39:22 +0300

    mvn clean install

--- service/src/test/java/greencity/ModelUtils.java
+++ service/src/test/java/greencity/ModelUtils.java
@@ -258,48 +258,48 @@ public class ModelUtils {
 
     public static UserVO getUserVOWithData() {
         return UserVO.builder()
-                .id(13L)
-                .name("user")
-                .email("namesurname1995@gmail.com")
-                .role(Role.ROLE_USER)
-                .userCredo("save the world")
-                .emailNotification(EmailNotification.MONTHLY)
-                .userStatus(UserStatus.ACTIVATED)
-                // .rating(13.4)
-                .verifyEmail(VerifyEmailVO.builder()
-                        .id(32L)
-                        .user(UserVO.builder()
-                                .id(13L)
-                                .name("user")
-                                .build())
-                        .token("toooookkkeeeeen42324532542")
-                        .build())
-                /*
-                 * .userFriends(Collections.singletonList( UserVO.builder() .id(75L)
-                 * .name("Andrew") .build()))
-                 */
-                .refreshTokenKey("refreshtoooookkkeeeeen42324532542")
-                .ownSecurity(null)
-                .dateOfRegistration(LocalDateTime.of(2020, 6, 6, 13, 47))
-                /*
-                 * .userLocationDto( new UserLocationDto(1L, "Lviv", "Львів", "Lvivska",
-                 * "Львівська", "Ukraine", "Україна", 20.000000, 20.000000))
-                 */
-                .showToDoList(ProfilePrivacyPolicy.PUBLIC)
-                .showEcoPlace(ProfilePrivacyPolicy.PUBLIC)
-                .showLocation(ProfilePrivacyPolicy.PUBLIC)
-                .ownSecurity(OwnSecurityVO.builder()
-                        .id(1L)
-                        .password("password")
-                        .user(UserVO.builder()
-                                .id(13L)
-                                .build())
-                        .build())
-                .lastActivityTime(LocalDateTime.of(2020, 12, 11, 13, 30))
-                .firstName("Julia")
-                .languageId(1L)
-                .socialNetworks(getSocialNetworkVOs())
-                .build();
+            .id(13L)
+            .name("user")
+            .email("namesurname1995@gmail.com")
+            .role(Role.ROLE_USER)
+            .userCredo("save the world")
+            .emailNotification(EmailNotification.MONTHLY)
+            .userStatus(UserStatus.ACTIVATED)
+            // .rating(13.4)
+            .verifyEmail(VerifyEmailVO.builder()
+                .id(32L)
+                .user(UserVO.builder()
+                    .id(13L)
+                    .name("user")
+                    .build())
+                .token("toooookkkeeeeen42324532542")
+                .build())
+            /*
+             * .userFriends(Collections.singletonList( UserVO.builder() .id(75L)
+             * .name("Andrew") .build()))
+             */
+            .refreshTokenKey("refreshtoooookkkeeeeen42324532542")
+            .ownSecurity(null)
+            .dateOfRegistration(LocalDateTime.of(2020, 6, 6, 13, 47))
+            /*
+             * .userLocationDto( new UserLocationDto(1L, "Lviv", "Львів", "Lvivska",
+             * "Львівська", "Ukraine", "Україна", 20.000000, 20.000000))
+             */
+            .showToDoList(ProfilePrivacyPolicy.PUBLIC)
+            .showEcoPlace(ProfilePrivacyPolicy.PUBLIC)
+            .showLocation(ProfilePrivacyPolicy.PUBLIC)
+            .ownSecurity(OwnSecurityVO.builder()
+                .id(1L)
+                .password("password")
+                .user(UserVO.builder()
+                    .id(13L)
+                    .build())
+                .build())
+            .lastActivityTime(LocalDateTime.of(2020, 12, 11, 13, 30))
+            .firstName("Julia")
+            .languageId(1L)
+            .socialNetworks(getSocialNetworkVOs())
+            .build();
     }
 
     public static UserProfileDtoRequest getUserProfileDtoRequest() {
@@ -721,60 +721,60 @@ public class ModelUtils {
 
     public static List<SocialNetwork> getSocialNetworks() {
         SocialNetwork socialNetwork1 = SocialNetwork.builder()
-                .id(9L)
-                .url("http://test.com.ua")
-                .user(getUser())
-                .socialNetworkImage(getOneSocialNetworkImage())
-                .build();
+            .id(9L)
+            .url("http://test.com.ua")
+            .user(getUser())
+            .socialNetworkImage(getOneSocialNetworkImage())
+            .build();
 
         SocialNetwork socialNetwork2 = SocialNetwork.builder()
-                .id(10L)
-                .url("http://test-test.com.ua")
-                .user(getUser())
-                .socialNetworkImage(getOneSocialNetworkImage())
-                .build();
+            .id(10L)
+            .url("http://test-test.com.ua")
+            .user(getUser())
+            .socialNetworkImage(getOneSocialNetworkImage())
+            .build();
 
         return List.of(socialNetwork1, socialNetwork2);
     }
 
     public static List<SocialNetworkVO> getSocialNetworkVOs() {
         SocialNetworkVO socialNetworkVO1 = SocialNetworkVO.builder()
-                .id(9L)
-                .url("http://test.com.ua")
-                .user(getUserVoShort())
-                .socialNetworkImage(getOneSocialNetworkImageVO())
-                .build();
+            .id(9L)
+            .url("http://test.com.ua")
+            .user(getUserVoShort())
+            .socialNetworkImage(getOneSocialNetworkImageVO())
+            .build();
 
         SocialNetworkVO socialNetworkVO2 = SocialNetworkVO.builder()
-                .id(10L)
-                .url("http://test-test.com.ua")
-                .user(getUserVoShort())
-                .socialNetworkImage(getOneSocialNetworkImageVO())
-                .build();
+            .id(10L)
+            .url("http://test-test.com.ua")
+            .user(getUserVoShort())
+            .socialNetworkImage(getOneSocialNetworkImageVO())
+            .build();
 
         return List.of(socialNetworkVO1, socialNetworkVO2);
     }
 
     public static SocialNetworkImageVO getOneSocialNetworkImageVO() {
         return SocialNetworkImageVO.builder()
-                .id(13L)
-                .imagePath("http://test-test.com.ua")
-                .hostPath("hostPath2")
-                .build();
+            .id(13L)
+            .imagePath("http://test-test.com.ua")
+            .hostPath("hostPath2")
+            .build();
     }
 
     public static SocialNetworkImage getOneSocialNetworkImage() {
         return SocialNetworkImage.builder()
-                .id(13L)
-                .imagePath("http://test-test.com.ua")
-                .hostPath("hostPath2")
-                .build();
+            .id(13L)
+            .imagePath("http://test-test.com.ua")
+            .hostPath("hostPath2")
+            .build();
     }
 
     public static UserVO getUserVoShort() {
         return UserVO.builder()
-                .id(1L)
-                .email("taras@gmail.com")
-                .build();
+            .id(1L)
+            .email("taras@gmail.com")
+            .build();
     }
 }
