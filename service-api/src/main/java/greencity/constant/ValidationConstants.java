862aef459d8d0ae30bad5b9e54979f2819dc00aa
Maksym Kozak <bersercer70@gmail.com>
2025-04-28 19:45:02 +0300

    allow username to start with a lowercase letter

--- service-api/src/main/java/greencity/constant/ValidationConstants.java
+++ service-api/src/main/java/greencity/constant/ValidationConstants.java
@@ -22,7 +22,7 @@ public class ValidationConstants {
     public static final int PLACE_NAME_MAX_LENGTH = 30;
     public static final String USERNAME_REGEXP = """
         ^(?!.*\\.\\.)(?!.*\\.$)(?!.*\\-\\-)\
-        (?=[ЄІЇҐЁА-ЯA-Z])\
+        (?=[ЄІЇҐЁєіїґёА-Яа-яA-Za-z])\
         [ЄІЇҐЁєіїґёА-Яа-яA-Za-z0-9\\s\\-'\\"’.ʼ]\
         {1,30}\
         (?<![ЭэЁёъЪЫы])$\
