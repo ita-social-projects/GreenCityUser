25c609d39e045d7b5bbaac1a487e5197a05b2c4c
Maksym Kozak <bersercer70@gmail.com>
2025-04-29 18:13:49 +0300

    add @Aspect logger of execution time of repository methods

---
+++ service-api/src/main/java/greencity/RepositoryMethodListener.java
@@ -0,0 +1,29 @@
+package greencity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class RepositoryMethodListener {
+
+    @Around("execution(* greencity.repository..*.*(..))")
+    public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long endTime = System.currentTimeMillis();
+            long executionTime = endTime - startTime;
+            log.info("Repository: {}, method: {}, execution time: {}", className, methodName, executionTime);
+        }
+    }
+}
