524888c7c602347e435791392c97a18c19a7c35d
Maksym Kozak <bersercer70@gmail.com>
2025-05-06 01:13:31 +0300

    expose language related endpoints that will be used by greencity

---
+++ service-api/src/main/java/greencity/service/LanguageService.java
@@ -0,0 +1,39 @@
+package greencity.service;
+
+import greencity.dto.language.LanguageVO;
+
+import java.util.List;
+
+public interface LanguageService {
+
+    /**
+     * Method to get all languages as {@link LanguageVO}
+     *
+     * @return {@link List} of {@link LanguageVO}
+     */
+    List<LanguageVO> getAllLanguages();
+
+    /**
+     * Find language {@link LanguageVO} by code
+     *
+     * @param code language code
+     * @return language {@link LanguageVO}
+     */
+    LanguageVO findLanguageByCode(String code);
+
+    /**
+     * Find language {@link LanguageVO} by id
+     *
+     * @param id language id
+     * @return language {@link LanguageVO}
+     */
+    LanguageVO findLanguageById(Long id);
+
+    /**
+     * Method to get all language codes
+     *
+     * @return {@link List} of {@link String} language codes
+     */
+    List<String> findAllLanguageCodes();
+
+}
