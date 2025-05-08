1f11d521275f38b2af284f3de94d8a23698328a3
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-05-08 22:42:47 +0300

    Changes in try-catch

--- service/src/main/java/greencity/service/UserServiceImpl.java
+++ service/src/main/java/greencity/service/UserServiceImpl.java
@@ -1088,9 +1088,11 @@ public class UserServiceImpl implements UserService {
                 .name(newUser.getName())
                 .profilePicturePath(profilePicture)
                 .build());
-        } catch (WebClientRequestException | WebClientResponseException e) {
+        } catch (WebClientRequestException e ) {
             log.warn("GreenCity service is unavailable: {}", e.getMessage());
-        } catch (RuntimeException e) {
+        } catch (WebClientResponseException e) {
+            log.warn("Bad response from GreenCity: {}", e.getMessage());
+        } catch (Exception e) {
             log.error("Unexpected error when calling GreenCity: {}", e.getMessage(), e);
         }
     }
