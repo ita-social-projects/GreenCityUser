05d015a8a2cbd0c7dad78a99162c22eaff962742
Maksym Kozak <bersercer70@gmail.com>
2025-05-08 20:18:31 +0300

    add tests for new mappers

---
+++ service/src/test/java/greencity/mapping/UserForListDtoMapperTest.java
@@ -0,0 +1,48 @@
+package greencity.mapping;
+
+import greencity.ModelUtils;
+import greencity.TestConst;
+import greencity.client.GreenCityRemoteClient;
+import greencity.dto.user.UserForListDto;
+import greencity.entity.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserForListDtoMapperTest {
+
+    @Mock
+    GreenCityRemoteClient greenCityRemoteClient;
+
+    @InjectMocks
+    UserForListDtoMapper userForListDtoMapper;
+
+    @Test
+    void convertTest() {
+        User user = ModelUtils.getUser();
+        Long userId = user.getId();
+        String userCredo = TestConst.CREDO;
+        UserForListDto expectedResult = UserForListDto.builder()
+                .id(userId)
+                .name(user.getName())
+                .dateOfRegistration(user.getDateOfRegistration())
+                .email(user.getEmail())
+                .userStatus(user.getUserStatus())
+                .role(user.getRole())
+                .userCredo(userCredo)
+                .build();
+
+        when(greenCityRemoteClient.findUserCredoByUserId(userId))
+                .thenReturn(userCredo);
+
+        UserForListDto actualResult = userForListDtoMapper.convert(user);
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
