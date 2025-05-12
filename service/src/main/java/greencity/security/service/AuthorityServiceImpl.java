5db5c97b5aa9b528ff8baa452250ac4c2a8b0ef5
ViktoriyaRyazhska <vryazhskaya@gmail.com>
2025-05-12 14:17:15 +0000

    Deploying to gh-pages from  @ 47691ee91fb151d3adf28508550532f980672963 🚀

--- service/src/main/java/greencity/security/service/AuthorityServiceImpl.java
+++ service/src/main/java/greencity/security/service/AuthorityServiceImpl.java
@@ -60,7 +60,7 @@ public class AuthorityServiceImpl implements AuthorityService {
             () -> new UsernameNotFoundException(ErrorMessage.USER_NOT_FOUND_BY_EMAIL + dto.getEmail()));
 
         List<String> positionNames = dto.getPositions().stream()
-            .map(PositionDto::getName).toList();
+            .map(PositionDto::getNameUk).toList();
 
         List<Position> positions = positionRepo.findPositionsByNames(positionNames);
         List<Authority> list = authorityRepo.findAuthoritiesByPositions(positionNames);
