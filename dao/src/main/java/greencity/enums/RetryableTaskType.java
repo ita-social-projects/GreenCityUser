5d96328ca2aa8d21a94b04ec3d02d41261339c3d
Olexandr <Mega.alex.alex2000@gmail.com>
2025-06-19 15:47:46 +0300

    8321: Create RetryableTask entity for transactional outbox

---
+++ dao/src/main/java/greencity/enums/RetryableTaskType.java
@@ -0,0 +1,11 @@
+package greencity.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RetryableTaskType {
+    SEND_GREENCITY_REQUEST
+
+}
