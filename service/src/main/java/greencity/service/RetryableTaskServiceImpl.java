e076898a67351053c45f8b5c9fb0f9266ce343bc
Olexandr <Mega.alex.alex2000@gmail.com>
2025-06-23 17:25:48 +0300

    8321: add feature to get retryable task from db and add usage in user service

--- service/src/main/java/greencity/service/RetryableTaskServiceImpl.java
+++ service/src/main/java/greencity/service/RetryableTaskServiceImpl.java
@@ -5,9 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import greencity.entity.RetryableTask;
 import greencity.enums.RetryableTaskStatus;
 import greencity.enums.RetryableTaskType;
+import greencity.exception.exceptions.RetryableTaskSerializationException;
 import greencity.repository.RetryableTaskRepository;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,23 +23,38 @@ import org.springframework.transaction.annotation.Transactional;
 public class RetryableTaskServiceImpl implements RetryableTaskService {
     private final RetryableTaskRepository retryableTaskRepository;
     private final ObjectMapper objectMapper;
+    private static final Integer LIMIT = 100;
+    private static final Integer TIMEOUTINSECONDS = 60;
 
     @Transactional
-    public <T> void saveRetryableTask(T payloadObject) {
+    public <T> void saveRetryableTask(T payloadObject, RetryableTaskType type) {
         try {
             String jsonPayload = objectMapper.writeValueAsString(payloadObject);
 
             RetryableTask task = RetryableTask.builder()
                 .payload(jsonPayload)
-                .type(RetryableTaskType.SEND_GREENCITY_REQUEST)
+                .type(type)
                 .status(RetryableTaskStatus.IN_PROGRESS)
+                .retryTime(LocalDateTime.now())
                 .build();
 
             retryableTaskRepository.save(task);
-            log.info("Saved retryable task of type: {}", RetryableTaskType.SEND_GREENCITY_REQUEST);
+            log.info("Saved retryable task of type: {}", type);
         } catch (JsonProcessingException e) {
             log.error("Failed to serialize object to JSON", e);
-            throw new RuntimeException("Failed to serialize payload", e);
+            throw new RetryableTaskSerializationException("Failed to serialize payload", e);
         }
+    }
+
+    @Transactional
+    public List<RetryableTask> getRetryableTaskForProcessing(RetryableTaskType type) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        Pageable pageable = PageRequest.of(0, LIMIT);
+        List<RetryableTask> retryableTasks = retryableTaskRepository.findRetryableTaskForProcessing(
+            type, LocalDateTime.now(), RetryableTaskStatus.IN_PROGRESS, pageable);
+        for (RetryableTask task : retryableTasks) {
+            task.setRetryTime(currentTime.plus(Duration.ofSeconds(TIMEOUTINSECONDS)));
+        }
+        return retryableTasks;
     }
 }
