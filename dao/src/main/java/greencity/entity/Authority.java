e527af70c926f3c118cd86916482c0ee2bbf95f0
olehio-p <oleh.popovych.shi.2023@lpnu.ua>
2025-08-20 15:29:55 +0300

    Add new changelog to create authorities categories

--- dao/src/main/java/greencity/entity/Authority.java
+++ dao/src/main/java/greencity/entity/Authority.java
@@ -1,12 +1,21 @@
 package greencity.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.*;
 import java.util.List;
 
 @NoArgsConstructor
@@ -25,7 +34,7 @@ public class Authority {
     @Column
     private String name;
 
-    @ManyToMany(mappedBy = "authorities", cascade = CascadeType.ALL)
+    @ManyToMany(mappedBy = "authorities", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private List<User> employees;
 
     @ManyToMany
