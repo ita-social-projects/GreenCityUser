472a55cf30f869ee578c46eacabd54b4981bd3e2
BohdanLys <mrboba04@gmail.com>
2025-09-17 16:16:39 +0300

    Removed all functionality related to GreenCityChat

--- service/src/test/java/greencity/service/UserServiceImplTest.java
+++ service/src/test/java/greencity/service/UserServiceImplTest.java
@@ -3,7 +3,6 @@ package greencity.service;
 import greencity.ModelUtils;
 import greencity.TestConst;
 import greencity.client.GreenCityRemoteClient;
-import greencity.client.RestClient;
 import greencity.constant.ErrorMessage;
 import greencity.constant.UpdateConstants;
 import greencity.dto.CoordinatesDto;
@@ -111,9 +110,6 @@ class UserServiceImplTest {
 
     @Mock
     GreenCityRemoteClient greenCityRemoteClient;
-
-    @Mock
-    RestClient restClient;
 
     @Mock
     SimpMessagingTemplate messagingTemplate;
