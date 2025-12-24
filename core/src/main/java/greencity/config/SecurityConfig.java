204148a2a450b0ab11f4ce304ce1eaf8b5ae7bbd
Olexandr <Mega.alex.alex2000@gmail.com>
2025-12-24 09:39:23 +0200

    use another method to block the endpoint /actuator/prometheus

--- core/src/main/java/greencity/config/SecurityConfig.java
+++ core/src/main/java/greencity/config/SecurityConfig.java
@@ -133,6 +133,7 @@ public class SecurityConfig {
                     "/api/testers/sign-in")
                 .permitAll()
                 .requestMatchers(HttpMethod.GET, "/check-auth").permitAll()
+                .requestMatchers("/actuator/**").hasAnyRole(ADMIN)
                 .requestMatchers(HttpMethod.GET,
                     "/user/to-do-list-items/habits/{habitId}/to-do-list",
                     "/user/{userId}/{habitId}/custom-to-do-list-items/available",
